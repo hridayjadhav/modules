9c4a2517605bb7597e17d00a66b8591c1f7c7552
Hriday <hridayjadhav789@gmail.com>
2023-08-21 11:44:24 +0530

    modules

---
+++ script.js
@@ -0,0 +1,17 @@
+export default function newUSer(){
+    console.log('Hello World');
+}
+
+export function userTwo(){
+    console.log('User Two here');
+}
+
+
+export let userThree = 'User Three here';
+
+
+export class userClass{
+    classOne(){
+        console.log('User class is here');
+    }
+}
